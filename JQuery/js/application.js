f28687e2d9670d73b0a613815b5f477749986299
kevinlee971208 <66408656+kevinlee971208@users.noreply.github.com>
2020-09-07 16:02:40 +0800

    Add files via upload

--- JQuery/js/application.js
+++ JQuery/js/application.js
@@ -1,26 +1,68 @@
-const sum = function (total, x) {
-    return total + x;
-};
-
-let updateSubTotal = function (element) {
-    let prodPrice = parseFloat($(element).children('.productPrice').text());
-    let prodQuant = parseFloat($(element).children('.productQuantity').text());
+let updateSubTotal = function (ele) {
+    let prodPrice = parseFloat($(ele).find('.productPrice input').val());
+    let prodQuant = parseFloat($(ele).find('.productQuantity input').val());
 
     let subTotal = prodPrice * prodQuant;
-    $(element).children('.productSubTotal').html(subTotal);
+    $(ele).children('.productSubTotal').html(subTotal);
 
     return subTotal;
 }
 
+let sum = function (total, x) {
+    return total + x;
+};
 
-$(document).ready(function () {
+let updateShoppingCart = function () {
     let totalValue = [];
 
-    $('tbody tr').each(function (i, element) {
-        let cartSubTotals = updateSubTotal(element);
-        totalValue.push(cartSubTotals);
+    $('tbody tr').each(function (i, ele) {
+        let subTotal = updateSubTotal(ele);
+        console.log(subTotal);
+        totalValue.push(subTotal);
     });
 
     let shoppingCartTotal = totalValue.reduce(sum);
     $('#shoppingCartTotal').html(shoppingCartTotal);
+}
+
+$(document).ready(function () {
+    updateShoppingCart();
+
+    $(document).on('click', '.btn.remove', function (event) {
+        $(this).closest('tr').remove();
+        updateShoppingCart();
+    });
+
+    let timeout;
+    $(document).on('input', 'tr input', function () {
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+            updateShoppingCart();
+        }, 1000);
+    });
+
+
+    $('#addGrocery').on('submit', function (event) {
+        event.preventDefault();
+        let productName = $(this).children('[name=productName]').val();
+        console.log(productName)
+        let productPrice = $(this).children('[name=productPrice]').val();
+        console.log(productPrice);
+        let productQuantity = $(this).children('[name=productQuantity]').val();
+        console.log(productQuantity);
+
+        $('tbody').append(
+            '<tr>' +
+                '<td class="productName">' + productName + '</td>' +
+                '<td class="productPrice"><input type="number" value="' + productPrice + '" /></td>' +
+                '<td class="productQuantity"><input type="number" value="' + productQuantity + '" /></td>' +
+                '<td class="productSubTotal"></td>' +
+                '<td><button class="btn btn-light btn-sm remove">remove</button></td>' +
+            '</tr>');
+        
+        updateShoppingCart();
+        $(this).children('[name=productName]').val('');
+        $(this).children('[name=productPrice]').val('');
+        $(this).children('[name=productQuantity]').val('');
+    });
 });
